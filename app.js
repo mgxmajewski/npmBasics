2fcb0f0e6fa46be6aaa4fb3796536fbd142b7c35
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-11-30 01:55:49 +0100

    Adds colors package

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const myPlaintextPassword = "password";
 const bcrypt = require('bcrypt');
+const colors = require('colors');
 
 bcrypt.genSalt(10, function(err, salt) {
     bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
-        console.log(hash);
+        console.log(hash.green);
         // Store hash in your password DB.
     });
 });
